d9ac58d58cc04eeb1388ef9979f7729937054e8c
Anthony Romaniello <66272872+aromanielloNTIA@users.noreply.github.com>
2024-12-03 12:05:19 -0500

    Rename test_utils file

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,7 @@
-# TODO-TEMPLATE: Rename this file
 import csv
 from pathlib import Path
 
-import pytest
-
-# TODO-TEMPLATE: Import the python wrapper package
-from ITS import PropLibTemplate
-
-# Test data is expected to exist in extern/<TODO-TEMPLATE>-test-data
+# Test data is expected to exist in extern/TODO-TEMPLATE-test-data
 TEST_DATA_DIR = (Path(__file__).parent.parent / "extern") / "TODO-TEMPLATE"
 ABSTOL__DB = 0.1  # Absolute tolerance, in dB, to ensure outputs match expected value
 
@@ -18,10 +12,10 @@ def read_csv_test_data(filename: str):
         reader = csv.reader(f)
         next(reader)  # Skip header row
         for row in reader:
-            # yield (inputs, rtn, output)
+            # yields (*inputs, rtn, output)
             yield tuple(map(float, row[:-2])), int(row[-2]), float(row[-1])
 
 
-# TODO-TEMPLATE: Implement unit tests for this Python wrapper
+# TODO-TEMPLATE: Delete this dummy test and write your own in another file.
 def test_always_pass():
     return
